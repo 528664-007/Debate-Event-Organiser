8c435aec19ad561d964b39da57c61668ced6f432
Sameer <sameerfayaz1028@gmail.com>
2025-11-26 11:16:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-class Config: pass
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = "your-secret-key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database/debate.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
